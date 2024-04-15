d3b3c0c986384e12a2025ac4eebd7548e78422af
Paulo J. Veroneze <44308731+TioKyoto@users.noreply.github.com>
2024-04-14 23:47:14 -0300

    Add files via upload

--- CodigoCalcIMC.py
+++ CodigoCalcIMC.py
@@ -3,20 +3,20 @@ nome = ("Olá" , pergunta_nome)
 peso = float(input("Qual seu peso (em KG)? "))
 altura = float(input("Qual sua Altura (em CM)? "))
 
-altura_metros = altura / 100  
-imc = peso / (altura_metros ** 2)
+altura_cm = altura / 100  
+imc = peso / (altura_cm ** 2)
 
 print(pergunta_nome , (f"seu IMC é: {imc:.2f}"))
 
-if imc < 17:
+if imc < 18:
 	print(" Abaixo do Peso")
-elif imc < 18:
+elif imc < 24:
 	print("Peso Ideal ")
-elif imc < 25:
+elif imc < 29:
 	print("Sobrepeso")
-elif imc < 30:
+elif imc < 34:
 	print("Obesidade Grau 1.")
-elif imc < 35:
+elif imc < 39:
 	print("Obesidade Grau 2")
-elif imc < 40:
+elif imc < 45:
 	print("Obesidade Grau 3")
